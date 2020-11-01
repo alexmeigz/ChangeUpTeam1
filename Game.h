983d59c08e5443578afd5568dab818e84a0b7963
alrudolph <alex3r@me.com>
2020-10-31 19:52:15 -0700

    updates

--- Game.h
+++ Game.h
@@ -8,14 +8,14 @@ enum class Move {ADD, REMOVE};
 
 class Game {
 public:
-    Game(int size);
+    Game(int size, int maxBall);
     void printBoardState(std::string state) const;
     bool makeMove(Move move, X x, Y y);
+    bool finished(); // NEED TO CHECK WIN CONDITION - use gameboard layer method
     ~Game();
 private:
-    const int SIZE;
     GameBoard * gameboard;
     Player * player1;
     Player * player2;
-    bool turnTracker;
+    bool turnTracker; // WE HAVE TO SWITCH TURNS SOMEWHERE
 };
