1078b38a12f2e7f21d7bfb137b03e2296cbefa3a
alrudolph <alex3r@me.com>
2020-11-08 09:20:13 -0800

    gameboard class

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -1,8 +1,11 @@
 #include "GameBoard.h"
-#include "utilities.h"
 
-GameBoard::GameBoard(const int size) : SIZE(size) {
-    init2D<GoalPost>(this->board, this->SIZE);
+#include <iostream>
+GameBoard::GameBoard(const int size) : SIZE(size), board(size) {
+    for (int i = 0; i < this->SIZE; ++i) {
+        // set each element to a vector of size SIZE, passing in SIZE to the constructor of GoalPost
+        this->board[i] = std::vector<GoalPost>(this->SIZE, this->SIZE);
+    }
 }
 
 bool GameBoard::removeBall(int x, int y) {
@@ -13,19 +16,18 @@ bool GameBoard::addBall(int player, int x, int y) {
     return this->board[x][y].push(player);
 }
 
-std::vector<int> GameBoard::fillPost(std::vector<int> post) const {
+std::vector<int> GameBoard::fillPost(const std::vector<int> &post) const {
+    // copy over the filled in values
     std::vector<int> output = post;
-    
-    // probably a better way to do idk
-    while(output.size() != this->SIZE) {
-        output.push_back(0);
-    }
+
+    // fill in the rest with 0's
+    output.resize(this->SIZE, 0);
 
     return output;
 }
 
 Vector2D<int> GameBoard::getLayerX(int x) const {
-    Vector2D<int> output = init2D<int>(this->SIZE);
+    Vector2D<int> output(this->SIZE);
 
     for (int y = 0; y < this->SIZE; ++y) {
         std::vector<int> col = this->board[x][y].getPost();
@@ -36,7 +38,7 @@ Vector2D<int> GameBoard::getLayerX(int x) const {
 }
 
 Vector2D<int> GameBoard::getLayerY(int y) const {
-    Vector2D<int> output = init2D<int>(this->SIZE);
+    Vector2D<int> output(this->SIZE);
 
     for (int x = 0; x < this->SIZE; ++x) {
         std::vector<int> col = this->board[x][y].getPost();
@@ -47,7 +49,7 @@ Vector2D<int> GameBoard::getLayerY(int y) const {
 }
 
 Vector2D<int> GameBoard::getLayerZ(int z) const {
-    Vector2D<int> output = init2D<int>(this->SIZE);
+    Vector2D<int> output(this->SIZE);
 
     for (int x = 0; x < this->SIZE; ++x) {
         for (int y = 0; y < this->SIZE; ++y) {
