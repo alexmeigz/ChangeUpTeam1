0a7f0ca6776fc97e8f4385670f2ba1662dc0d1f3
alrudolph <alex3r@me.com>
2020-11-08 09:20:13 -0800

    gameboard class

--- GameBoard.h
+++ GameBoard.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <vector>
-#include "utilities.h"
+#include "GoalPost.h"
+
+template <class T>
+using Vector2D = std::vector<std::vector<T>>;
 
 class GameBoard {
 public:
@@ -12,8 +15,9 @@ public:
     Vector2D<int> getLayerX(int x) const;
     Vector2D<int> getLayerY(int y) const;
     Vector2D<int> getLayerZ(int z) const;
-public:
-    std::vector<int> fillPost(std::vector<int> post) const;
+private:
+    std::vector<int> fillPost(const std::vector<int> &post) const;
+    
     Vector2D<GoalPost> board;
     const int SIZE;
 };
