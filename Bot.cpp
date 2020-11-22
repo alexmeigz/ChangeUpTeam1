9f40a7317768337d45a4b7e98f2d288607958011
Matthew Ho <matt@Laurens-MacBook-Pro.local>
2020-11-21 17:14:57 -0800

    updates

--- Bot.cpp
+++ Bot.cpp
@@ -1,8 +1,7 @@
 #include "Bot.h"
 
-Move Bot::chooseMove(vector<pairs<int, int> > av_add, 
-	vector<pairs<int, int> > av_rem, 
-	vector<int> current_state){
+Move Bot::chooseMove(unordered_map<Move, vector<int> > possible_moves){
+	/*
 	vector<Move> possible_moves;
 	for(int i = 0; i < av_add.size(); i++){
 		Move dummy = {"ADD", av_add[i].first, av_add[i].second};
@@ -12,14 +11,34 @@ Move Bot::chooseMove(vector<pairs<int, int> > av_add,
 		Move dummy = {"REMOVE", av_rem[i].first, av_rem[i].second};
 		possible_moves.push_back(dummy);
 	}
+	*/
 
-	srand(time(0));
 	if((rand() % 100) <= exploration_rate * 100){
 		//make a random move
+		srand(time(0));
 		int move_index = rand() % possible_moves.size();
-		return possible_moves[move_index];
+		int counter = 0;
+		for(pair<Move, vector<int> option: possible_moves){
+			if(counter == move_index){
+				return option.first;
+			}
+			counter++;
+		}
 	}
-	
-	//choose greedy move
+
+	pair<Move, vector<int> > greedy_choice;
+	bool on_first_pair = true;
+	for(pair<Move, vector<int> option: possible_moves){
+		if(on_first_pair){
+			greedy_choice = option;
+			on_first_pair = false;
+		} else {
+			if(state_vals[option.second] > state_vals[greedy_choice.second]){
+				greedy_choice = option;
+			}
+		}
+	}
+
+	return greedy_choice.first;
 	
 }
