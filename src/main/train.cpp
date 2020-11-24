f9be19e5a09a1d913f195899dddd923f088971a8
Matthew Ho <msho@ucsb.edu>
2020-11-23 22:05:40 -0800

    added training timing

--- src/main/train.cpp
+++ src/main/train.cpp
@@ -1,8 +1,15 @@
+#pragma once
+
+#include <chrono>
 #include "../../include/ai/TheGym.h"
 
 int main() {
 	TheGym model;
 	//model.train(10);
-	model.train(1, true, false);
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	model.train(100, true, false);
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[micro sec]" << std::endl;
+
 	model.print();
 }
