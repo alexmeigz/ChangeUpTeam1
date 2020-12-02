45e4a769551d508dd5bfdd78148d74e23ccce9d6
Alex <alex3r@me.com>
2020-12-02 15:16:00 -0800

    updated states

--- src/main/train.cpp
+++ src/main/train.cpp
@@ -7,7 +7,7 @@ int main() {
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 	//train(rounds, quiet, start_fresh)
 	//model.train(1, true, true);
-	model.train(100, true, false);
+	model.train(10000, true, false);
 	std::cout << "finished training" << endl;
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	std::cout << "Time difference = " 
